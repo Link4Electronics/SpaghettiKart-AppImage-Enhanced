96b7b78462cf5b0262feec8cf38dd8f0a8abbcb6
Link4Electronics <link4electronics@gmail.com>
2026-02-03 14:43:30 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -60,9 +60,9 @@ install -m644 -t "/usr/bin" \
 #install -Dm644 yamls -t "/usr/bin"
 # Link executable to /usr/bin, add to desktop entry & icons
 #  ln -s "${SHIP_PREFIX}/Spaghettify" "${pkgdir}/usr/bin/Spaghettify"
-sed -i 's/^Icon=icon$/Icon=spaghettikart/' SpaghettiKart.desktop
+sed -i 's/^Icon=icon$/Icon=SpaghettiKart/' SpaghettiKart.desktop
 install -Dm644 "SpaghettiKart.desktop" -t "/usr/share/applications"
-install -Dm644 icon.png "/usr/share/pixmaps/spaghettikart.png"
+install -Dm644 icon.png "/usr/share/pixmaps/SpaghettiKart.png"
 # Licenses (HarbourMasters libraries are MIT, game engine + port source code is nonfree)
 install -Dm644 "libultraship/LICENSE" "/usr/share/licenses/spaghettikart/libultraship-LICENSE"
 install -Dm644 "torch/LICENSE" "/usr/share/licenses/spaghettikart/torch-LICENSE"
